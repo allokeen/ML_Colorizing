750063e3ce791dad2b4f1558722df42eb0a1a902
Nikola Świerczek <45972093+allokeen@users.noreply.github.com>
2024-06-20 12:48:33 +0200

    Removing initial files

--- main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
-import cv2
-import os
